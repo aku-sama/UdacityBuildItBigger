733169bee2249efc1a8274912ca5b3811e967149
diana.raspopova <raspopova@improveitgroup.com>
2017-04-22 10:52:21 +0700

    add ui test

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/MainActivityTest.java
@@ -0,0 +1,31 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by Diana.Raspopova on 4/22/2017.
+ */
+
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void ensureTextChangesWork() {
+        // Type text and then press the button.
+        onView(withId(R.id.jokeButton)).perform(click());
+
+        // Check that the text was not empty.
+        onView(withId(R.id.jokeText)).check(matches(isDisplayed()));
+    }
+}
